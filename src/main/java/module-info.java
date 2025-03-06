540a882e730541b64989006308d30d94d6b4f0e3
JOSIE_BROHM <josiembrohm@gmail.com>
2025-03-06 14:31:48 +0200

    restructuring

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mylibrary {
     requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.base;
 
     opens com.mylibrary to javafx.fxml;
     exports com.mylibrary;
